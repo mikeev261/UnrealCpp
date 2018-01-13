ec058093ef73a823b51991a6987ba15e29c47d45
mikeev261 <mikeev@gmail.com>
2018-01-13 12:36:19 -0500

    Initial CI

---
+++ Section_02/BullCowGame/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void PrintIntro();
+void PlayGame();
+string GetGuess();
+
+
+
+// the entry point for our application
+int main()
+{
+	PrintIntro();
+	PlayGame();
+	return 0; // exit the application
+}
+
+
+// introduce the game
+void PrintIntro()
+{
+	constexpr int WORLD_LENGTH = 9;
+	cout << "Welcome to Bulls and Cows, a fun word game.\n";
+	cout << "Can you guess the " <<  ;
+	cout << " letter isogram I'm thinking of?\n";
+	cout << endl;
+	return;
+}
+
+
+void PlayGame()
+{
+	// loop for the number of turns asking for guesses
+	constexpr int NUMBER_OF_TURNS = 5;
+	for (int count = 1; count <= NUMBER_OF_TURNS; count++) {
+		string Guess = GetGuess();
+		cout << "Your guess was: " << Guess << endl;
+		cout << endl;
+	}
+}
+
+
+string GetGuess()
+{
+	// get a guess from the player
+	cout << "Enter your guess: ";
+	string Guess = "";
+	getline(cin, Guess);
+	return Guess;
+}
+
+
